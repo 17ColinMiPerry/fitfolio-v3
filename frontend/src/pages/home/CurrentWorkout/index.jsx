1e1d0dcbb9b9deb47e3e25015f07412e2e0d10ec
17ColinMiPerry <17colinmiperry@gmail.com>
2025-04-28 16:54:30 -0700

    improved formatting of table

--- frontend/src/pages/home/CurrentWorkout/index.jsx
+++ frontend/src/pages/home/CurrentWorkout/index.jsx
@@ -236,7 +236,7 @@ export default function CurrentWorkout() {
         </table>
       </div>
 
-      <div className="mt-4">
+      <div className="flex justify-center items-center mt-2">
         <button
           onClick={() => createExercise("New Exercise")}
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
