9800556eef9240082910245dba453fc0f49d742d
17ColinMiPerry <17colinmiperry@gmail.com>
2025-04-30 22:54:44 -0700

    integrated shadcn support for UI

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header";
 import CurrentWorkout from "./CurrentWorkout";
 import WelcomeDashboard from "./WelcomeDashboard";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-300">
+      <Button>Click me</Button>
       <Header />
       <div className="flex flex-row items-center justify-center bg-gray-300 gap-12">
         <div className="w-[625px] h-[725px] rounded-lg">
